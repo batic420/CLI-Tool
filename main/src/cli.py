9ce2255cce341f809e4052c2202a8883e82fb7f3
batic420 <b.ludwig089765@gmail.com>
2025-02-08 13:30:05 +0100

    clean up cli.py and add new testing dir for first clone

--- main/src/cli.py
+++ main/src/cli.py
@@ -3,30 +3,42 @@
 from dotenv import load_dotenv
 import typer
 import os
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
+import shutil
 
 load_dotenv()
 app = typer.Typer()
 
 @app.command()
-def greet():
-    test = os.getenv("TEST")
-    print(f"Hello ${test}")
+def clone(
+    branch: str = typer.Option(
+        "main",
+        help="Branch to pull from",
+    )
+):
+    """Pull the latest changes from the root-kit repo"""
 
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Mr. {name}")
-    else:
-        print(f"Bye {name}")
+    try:
+        typer.echo(f"Cloning {os.getenv("REMOTE_URL")} on branch {branch}...")
 
-@app.command()
-def mdir(directory: str="default"):
-    print(os.getcwd())
+        if os.path.exists(os.getenv("LOCAL_DIR")):
+            typer.echo(f"Directory {os.getenv("LOCAL_DIR")} already exists. Deleting...")
+            shutil.rmtree(os.getenv("LOCAL_DIR"))
 
+        Repo.clone_from(
+            os.getenv("REMOTE_URL"), os.getenv("LOCAL_DIR"), branch=branch
+        )
 
-@app.command()
-def locate():
-    print(os.getcwd())
+        typer.echo(f"Cloning to {os.getenv("LOCAL_DIR")} complete!")
+    
+    except InvalidGitRepositoryError as e:
+        typer.echo(f"Invalid git repository: {e}")
+    except GitCommandError as e:
+        typer.echo(f"Git command error: {e}")
+    except Exception as e:
+        typer.echo(f"An unexpected error occurred: {e}")
+        
 
 if __name__ == "__main__":
     app()
