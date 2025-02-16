d8cc1fade87ad4f2ddf93d731642ec7c12e09c5e
batic420 <b.ludwig089765@gmail.com>
2025-02-16 15:09:21 +0100

    add basic docker commands for root-kit container

--- main/src/commands/docker.py
+++ main/src/commands/docker.py
@@ -24,3 +24,57 @@ def create(
         print(f"Container created!", result.stdout)
     except subprocess.CalledProcessError as e:
         print("An error occurred!", e.stderr)
+
+@cn.command()
+def delete(
+    serviceName: str = "mysql"
+):
+    """Delete a root-kit container"""
+
+    try:
+        result = subprocess.run(
+            ["docker", "compose", "down", serviceName],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        print(f"Container deleted!", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print("An error occurred!", e.stderr)
+
+@cn.command()
+def start(
+    serviceName: str = "mysql"
+):
+    """Start a root-kit container"""
+
+    try:
+        result = subprocess.run(
+            ["docker", "compose", "start", serviceName],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        print(f"Container started!", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print("An error occurred!", e.stderr)
+
+@cn.command()
+def stop(
+    serviceName: str = "mysql"
+):
+    """Stop a root-kit container"""
+
+    try:
+        result = subprocess.run(
+            ["docker", "compose", "stop", serviceName],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        print(f"Container stopped!", result.stdout)
+    except subprocess.CalledProcessError as e:
+        print("An error occurred!", e.stderr)
