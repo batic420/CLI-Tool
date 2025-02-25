d782ca4d4a683354b90388ef5055a5cf9e664b1e
batic420 <b.ludwig089765@gmail.com>
2025-02-25 21:48:40 +0100

    move and add new utils files for core functions like generating random service-nr

---
+++ src/root_kit/utils/template.py
@@ -0,0 +1,35 @@
+from jinja2 import Template
+
+template = Template("""
+services:
+  mysql-{{ custom_key }}:
+    image: mysql:8.0      
+    container_name: rootkit-mysql
+    ports:
+      - "${MYSQL_PORT}:3306"       
+    environment:
+      MYSQL_ROOT_PASSWORD: ${MYSQL_ROOT_PASSWORD}   
+      MYSQL_DATABASE: ${MYSQL_DATABASE}           
+      MYSQL_USER: ${MYSQL_USER}    
+      MYSQL_PASSWORD: ${MYSQL_PASSWORD}         
+    volumes:
+      - mysql_data:/var/lib/mysql    # Persist data even if the container restarts
+    healthcheck:
+      test: ["CMD", "mysqladmin", "ping", "-h", "localhost"]
+      interval: 5s
+      timeout: 10s
+      retries: 5
+
+volumes:
+  mysql_data:
+""")
+
+def generate_template(
+        custom_key: str,
+        file_path: str
+):
+    f = open(file_path, "x")
+
+    if f:
+        f.write(template.render(custom_key=custom_key))
+        f.close()
